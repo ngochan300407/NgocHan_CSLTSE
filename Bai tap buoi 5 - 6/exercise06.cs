3bfc41613195ad8de0707aa037c3237e736f387d
lehongochan <ngochan300407@gmail.com>
2026-09-21 21:35:47 +0700

    bài 1 tính tổng 2 số nguyên

--- Bai tap buoi 5 - 6/exercise06.cs
+++ Bai tap buoi 5 - 6/exercise06.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Channels;
+using NgocHan_CSLT.Bai_tap_3_4;
 
 namespace NgocHan_CSLT.Bai_tap_buoi_5___6
 {
     internal class exercise06
     {
+        public static void Main()
+        {
+            ex01();
+        }
+
+        static void ex01()
+        {
+            int ketqua = tong(3, 6);
+
+            Console.WriteLine($"Tong hai so nguyen = {ketqua} ");
+        }
+
+        static int tong(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using NgocHan_CSLT.Bai_tap_3___4;
 using NgocHan_CSLT.Bai_tap_buoi_5___6;
 
-exercise05.Main();
+exercise06.Main();
 
 
 
